9fa8d6bb8448cdae73493a5d9d03a2d7c29c4e3b
heenashree <heenashree2010@gmail.com>
2019-07-17 17:24:55 +0530

    PythonCodes OOPs DataAlgorithms

---
+++ DataAlgorithms/floordivision.py
@@ -0,0 +1,18 @@
+#The // operator performs integer (floor) division and the / operator performs float (true) division. An example for floor division is 50 // 11 = 4. An example for true division is 50 / 11 = 4.545454545454546.
+
+#Note that floor division always rounds ''down'', i.e., 3 // 2 == 1 and -3 // 2 == -2.
+
+#Although the puzzle seems simple, more than twenty percent of the Finxter users cannot solve it.
+#Python 2.7 and other upcoming version of python:
+
+#Division (/)
+#Divides left hand operand by right hand operand
+
+#Example: 4 / 2 = 2
+
+#Floor Division (//)
+#The division of operands where the result is the quotient in which the digits after the decimal point are removed. But if one of the operands is negative, the result is floored, i.e., rounded away from zero (towards negative infinity):
+
+#Examples: 9//2 = 4 and 9.0//2.0 = 4.0, -11//3 = -4, -11.0//3 = -4.0
+
+#Both / Division and // floor division operator are operating in similar fashion.
