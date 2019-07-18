34e3f926a35d31aa5715461c3b03b9c43b76e2c7
heenashree <heenashree2010@gmail.com>
2019-07-18 12:27:16 +0530

    PythonCodes OOPs DataAlgorithms

---
+++ DataAlgorithms/Did you know/allinOne.py
@@ -0,0 +1,25 @@
+#geekforgeeks
+print type(type(int))
+print(type(int))
+print(int)
+#Explanation: The type() function returns the class of the argument the object belongs to.
+# Thus, type(int) returns which is of the type 'type' object.
+
+
+print(chr(ord('A')))
+#Explanation: ord() function converts a character into its ASCII notation and chr() converts the ASCII to character.
+
+x = 'py' 'thon'
+
+#Prints python
+
+print(id(int))
+
+#Explanation: ord() function converts a character into its ASCII notation and chr() converts the ASCII to character.
+
+y = 8
+z = lambda x : x * y
+print z(6)
+#output is 48
+# Explanation: lambdas are concise functions and thus, result = 6 * 8
+
